9d69a9089a89dde02c37af1501ab6a9938ee0776
UtsavJain1221 <62596506+UtsavJain1221@users.noreply.github.com>
2023-08-14 13:55:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from '../Screens/Login' ;
+import Transaction from '../Screens/Transaction';
+
+import {Text,View,StyleSheets} from 'react-native';
+
+export default class App extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+    }
+}
